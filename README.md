256ddf929fe1e8f21ab77b86517c5db00e75ca6c
Mike Capper <mikebcapp@gmail.com>
2013-10-16 21:52:21 +1300

    Set up Git again due to error

---
+++ README.md
@@ -0,0 +1,6 @@
+# One Month Rails
+
+This is the pininteresting sample application for
+(*One Month Rails*)(http://onemonthrails.com)
+
+by (Mike Capper)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pininteresting::Application.config.secret_key_base = 'fc75c0705b9fb67620c29f549ae13291cbcb0ec84dc2d082537090cff1462d0b7a66127d7ca270747db2d28c6c5df606d2802d0cfb0509629931bfee4810328c'
